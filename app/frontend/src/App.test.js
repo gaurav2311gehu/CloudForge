120f05b0deb80bc3f40b95977d70839d5cb232ac
Anarion Technologies <sainig273@gmail.com>
2026-07-17 22:04:58 +0530

    Testing Pipeline

--- app/frontend/src/App.test.js
+++ app/frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders User Manager app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/User Manager/i)).toBeInTheDocument();
+  expect(screen.getByPlaceholderText(/Enter name/i)).toBeInTheDocument();
+  expect(screen.getByText(/Add/i)).toBeInTheDocument();
 });
